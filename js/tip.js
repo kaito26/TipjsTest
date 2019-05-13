21214e2e729f221b14fba6013733c86460f13668
kaito26 <kaitosan268@gmail.com>
2019-05-13 16:27:20 +0630

    vue test

--- js/tip.js
+++ js/tip.js
@@ -31,8 +31,6 @@ function calculateTip() {
   document.getElementById("totalTip").style.display = "block";
   document.getElementById("tip").innerHTML = total;
 
- 
-
   
 
 }
@@ -46,4 +44,33 @@ document.getElementById("calculate").onclick = function() {
   calculateTip();
 
 };
+
+// Load google charts
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+// Draw the chart and set the chart values
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+  ['Task', 'Hours per Day'],
+  ['Work', 8],
+  ['Learn', 2],
+  ['TV', 2],
+  ['eat', 2],
+  ['Sleep', 8]
+]);
+
+  // Optional; add a title and set the width and height of the chart
+  var options = {'title':'My Average Day', 'width':550, 'height':400};
+
+  // Display the chart inside the <div> element with id="piechart"
+  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+  chart.draw(data, options);
+}
+
+
+var myObject = new Vue({
+    el: '#app',
+  data: {message: 'Hello customer!'}
+})
 
